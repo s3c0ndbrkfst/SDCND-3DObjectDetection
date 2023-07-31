1a73afe2cf196a81e47f1a06570958edb08bda10
Nic Olbrantz <48780475+s3c0ndbrkfst@users.noreply.github.com>
2023-07-31 14:10:25 -0400

    initial attempt at ID_S3_EX1

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -25,6 +25,7 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 # model-related
 from tools.objdet_models.resnet.models import fpn_resnet
 from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing 
+from tools.objdet_models.resnet.utils.torch_utils import _sigmoid
 
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
@@ -34,20 +35,24 @@ from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v
 def load_configs_model(model_name='darknet', configs=None):
 
     # init config file, if none has been passed
-    if configs==None:
-        configs = edict()  
+    if configs == None:
+        configs = edict()
 
     # get parent directory of this file to enable relative paths
     curr_path = os.path.dirname(os.path.realpath(__file__))
-    parent_path = configs.model_path = os.path.abspath(os.path.join(curr_path, os.pardir))    
-    
+    parent_path = configs.model_path = os.path.abspath(
+        os.path.join(curr_path, os.pardir))
+
     # set parameters according to model type
     if model_name == "darknet":
-        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'darknet')
-        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'complex_yolov4_mse_loss.pth')
+        configs.model_path = os.path.join(
+            parent_path, 'tools', 'objdet_models', 'darknet')
+        configs.pretrained_filename = os.path.join(
+            configs.model_path, 'pretrained', 'complex_yolov4_mse_loss.pth')
         configs.arch = 'darknet'
         configs.batch_size = 4
-        configs.cfgfile = os.path.join(configs.model_path, 'config', 'complex_yolov4.cfg')
+        configs.cfgfile = os.path.join(
+            configs.model_path, 'config', 'complex_yolov4.cfg')
         configs.conf_thresh = 0.5
         configs.distributed = False
         configs.img_size = 608
@@ -58,12 +63,48 @@ def load_configs_model(model_name='darknet', configs=None):
         configs.use_giou_loss = False
 
     elif model_name == 'fpn_resnet':
-        ####### ID_S3_EX1-3 START #######     
+        ####### ID_S3_EX1-3 START #######
         #######
         print("student task ID_S3_EX1-3")
+        configs.model_path = os.path.join(
+            parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(
+            configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+        configs.saved_fn = 'fpn_resnet'
+        configs.arch = 'fpn_resnet'
+        configs.K =50
+        configs.num_samples = None
+        configs.num_workers = 1
+        configs.batch_size = 1
+        configs.peak_thresh = 0.2
+        configs.conf_thresh = 0.5
 
-        #######
-        ####### ID_S3_EX1-3 END #######     
+        configs.pin_memory = True
+        configs.distributed = False  # For testing on 1 GPU only
+
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
+    #######
+    ####### ID_S3_EX1-3 END #######
 
     else:
         raise ValueError("Error: Invalid model name")
@@ -114,13 +155,18 @@ def create_model(configs):
     
     elif 'fpn_resnet' in configs.arch:
         print('using ResNet architecture with feature pyramid')
-        
-        ####### ID_S3_EX1-4 START #######     
+
+        ####### ID_S3_EX1-4 START #######
         #######
         print("student task ID_S3_EX1-4")
-
+        model = fpn_resnet.get_pose_net(
+            num_layers=18,
+            heads=configs.heads,
+            head_conv=configs.head_conv,
+            imagenet_pretrained=configs.imagenet_pretrained,
+        )
         #######
-        ####### ID_S3_EX1-4 END #######     
+        ####### ID_S3_EX1-4 END #######
     
     else:
         assert False, 'Undefined model backbone'
@@ -163,13 +209,22 @@ def detect_objects(input_bev_maps, model, configs):
 
         elif 'fpn_resnet' in configs.arch:
             # decode output and perform post-processing
-            
-            ####### ID_S3_EX1-5 START #######     
+
+            ####### ID_S3_EX1-5 START #######
             #######
             print("student task ID_S3_EX1-5")
-
+            outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
+            outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
+            # detections size (batch_size, K, 10)
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
+                                outputs['dim'], K=configs.K)
+            detections = detections.cpu().numpy().astype(np.float32)
+            detections = post_processing(
+                detections, configs)
+            detections = detections[0][1]
+            print(f"detections: {detections}")
             #######
-            ####### ID_S3_EX1-5 END #######     
+            ####### ID_S3_EX1-5 END #######
 
             
 
